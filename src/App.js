7a0d53c92b793f476a8bfcf567d2e1e96f3038ae
Paul Tinca <Paul.Tinca@solera.com>
2022-11-10 16:28:14 +0100

    make button work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import Group from "./Components/Group";
+import GroupDetails from "./Components/GroupDetails";
+import {useState} from 'react';
+const task = ["HTML", "React", "JS", "c++"];
+const tasksScore = [11, 22, 33, 41];
 
-const App = (props) => {
-  
-  const clickHandler = () => {
-    console.log("click done!")
-  }
+export default function App(){
+  const [isShown, setIsShown] = useState(false);
+  const handleClick = event => {
+    // 👇️ toggle shown state
+    setIsShown(current => !current);
+
+    // 👇️ or simply set it to true
+    // setIsShown(true);
+  };
 
   return <div className="app">
 
       <h1>Solera Teams</h1>
       <div>
           <div>Groups Component</div>
-          <button onClick={clickHandler}>
-            <Group name="Group 1" points="15" assignment="2"/>
-          </button>
+          <button onClick={handleClick}><Group name="Group 1" points="15" assignment="4"/></button>
+  
+       
       </div>
+      
+      
+         
+
+{/* 👇️ show elements on click */}
+{isShown && (
+  <div>
+    <div>Groups Details</div>
+     <GroupDetails names="Group 1" pointss="15" tasks={task} tasksScores = {tasksScore} />
+  </div>
+)}
+
+{/* 👇️ show component on click */}
+{isShown && <Box />};
   </div>
 }
 
-export default App;
+function Box() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+
+
