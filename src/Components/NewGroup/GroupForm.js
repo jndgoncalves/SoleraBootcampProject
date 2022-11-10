b7be9db822ea5293d096bc0917195ce533ddab29
ntaquelim <NunoJoao.Sousa@solera.com>
2022-11-10 16:50:14 +0100

    css changes and stuff

--- src/Components/NewGroup/GroupForm.js
+++ src/Components/NewGroup/GroupForm.js
@@ -1,14 +1,14 @@
-
+import "./GroupForm.css";
 
 const groupForm = () => {
     return <form>
-         <div>
-            <label>Group name:</label>  
-            <input type="text"></input>
-            <label>Points:</label>
-            <input type="number"></input>
-            <label>Assignments:</label>
-            <input type="number"></input>
+         <div id="textDiv">
+            <label for="text">Group name:</label>  
+            <input type="text" min="1" required="0"></input>
+            <label for="number">Points:</label>
+            <input type="number" min="0" id="point" required="0"></input>
+            <label for="number">Assignments:</label>
+            <input type="number" min="0" id="assignments" required="0"></input>
         </div>
         <div >
             <button type="submit">Add Group</button>
