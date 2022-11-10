185b8a918beff3f03b55cb75d7580a7890ae055c
ntaquelim <NunoJoao.Sousa@solera.com>
2022-11-10 16:10:30 +0100

    css styles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import Group from "./Components/Group/Group";
 import "./App.css";
 import NewGroup from "./Components/NewGroup/NewGroup.js";
 
+
 const App = (props) => {
   
   const clickHandler = () => {
     console.log("click done!")
   }
 
-  return <div className="App">
-      <h1>Solera Teams</h1>
-      <NewGroup />
-      <div className="BigDiv">
-          <button onClick={clickHandler}>
+  return <div className="App" id="App">
+      <h1>Solera Teams Scores</h1>
+      <div class="BigDiv" id="BigDiv">
+          <button onClick={clickHandler} >
             <Group name="Group 1" points="15" assignment="2"/>
           </button>
+          <button onClick={clickHandler}>
+            <Group name="Group 2" points="20" assignment="3"/>
+          </button>
+          <button onClick={clickHandler}>
+            <Group name="Group 3" points="150" assignment="7"/>
+          </button>
+          <button onClick={clickHandler}>
+            <Group name="Group 3" points="150" assignment="7"/>
+          </button>
+          <button onClick={clickHandler}>
+            <Group name="Group 3" points="150" assignment="7"/>
+          </button>
       </div>
+      <NewGroup />
   </div>
 }
 
