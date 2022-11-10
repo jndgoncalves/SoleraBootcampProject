29a79e80f8ccb0ba4c3797b3c83f9d7da4234ade
Conor Fairclough <conorjack.fairclough@solera.com>
2022-11-10 16:54:49 +0100

    Add GroupCard & CSS

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import Group from "./Components/Group/Group";
+import GroupCard from "./Components/Group/GroupCard";
 import "./App.css";
 import NewGroup from "./Components/NewGroup/NewGroup.js";
 
-
 const App = (props) => {
-  
+
   const clickHandler = () => {
     console.log("click done!")
   }
 
-  return <div className="App" id="App">
-      <h1>Solera Teams Scores</h1>
-      <div class="BigDiv" id="BigDiv">
-          <button onClick={clickHandler} >
-            <Group name="Group 1" points="15" assignment="2"/>
-          </button>
-          <button onClick={clickHandler}>
-            <Group name="Group 2" points="20" assignment="3"/>
-          </button>
-          <button onClick={clickHandler}>
-            <Group name="Group 3" points="150" assignment="7"/>
-          </button>
-          <button onClick={clickHandler}>
-            <Group name="Group 3" points="150" assignment="7"/>
-          </button>
-          <button onClick={clickHandler}>
-            <Group name="Group 3" points="150" assignment="7"/>
-          </button>
-      </div>
+  return <div className="App">
+      <h1>Solera Teams</h1>
       <NewGroup />
+      <div className="BigDiv">
+          <GroupCard name="Group 1" points="100" assignment="2"></GroupCard>
+          <GroupCard name="Group 2" points="100" assignment="2"></GroupCard>
+      </div>
   </div>
 }
 
