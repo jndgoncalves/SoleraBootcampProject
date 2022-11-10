b7ea7fc8e6691b121b2a855a25df15561574756d
Andrea Colucci <Andrea.Colucci@solera.com>
2022-11-10 12:09:59 +0100

    add import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Group from "./Components/Group";
+import "./App.css";
 
 const App = (props) => {
   
   const clickHandler = () => {
     console.log("click done!")
+  }
+
+  
+  const submitHandler = (event) => {
+    
+    const groupData = {
+      name : enteredName,
+      points : 0,
+      assignment : 0
+    }
   }
 
   return <div className="app">
