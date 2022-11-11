ae6e8ca94c53ded14a08ac1ee3eac89dbc473663
Paul Tinca <Paul.Tinca@solera.com>
2022-11-11 10:27:56 +0100

    make buttons functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Group from "./Components/Group";
 import GroupDetails from "./Components/GroupDetails";
 import {useState} from 'react';
-const task = ["HTML", "React", "JS", "c++"];
-const tasksScore = [11, 22, 33, 41];
-
+const task1 = ["HTML", "React", "JS", "c++"];
+const tasksScore1 = [11, 22, 33, 41];
+const task2 = ["HTML", "React", "JS", "c++"];
+const tasksScore2 = [44, 14, 54, 56];
+const task3 = ["HTML", "React", "JS", "c++"];
+const tasksScore3 = [77, 33, 76, 99];
+let idNumber
 export default function App(){
   const [isShown, setIsShown] = useState(false);
-  let idNumber
+
   const handleClick = event => {
     // 👇️ toggle shown state
     setIsShown(current => !current);
     idNumber=event.currentTarget.id;
-    console.log(idNumber);
+    // console.log(idNumber);
 
     // 👇️ or simply set it to true
     // setIsShown(true);
@@ -22,9 +26,9 @@ export default function App(){
       <h1>Solera Teams</h1>
       <div>
           <div>Groups Component</div>
-          <button onClick={handleClick} id={'1'}><Group name="Group 1" points="15" assignment="4"/></button>
-          <button onClick={handleClick} id={'2'}><Group name="Group 2" points="25" assignment="2"/></button>
-          <button onClick={handleClick} id={'3'}><Group name="Group 3" points="5" assignment="3"/></button>
+          <button onClick={handleClick} id="1"><Group name="Group 1" points="15" assignment="4"/></button>
+          <button onClick={handleClick} id='2'><Group name="Group 2" points="25" assignment="2"/></button>
+          <button onClick={handleClick} id='3'><Group name="Group 3" points="5" assignment="3"/></button>
   
        
       </div>
@@ -36,14 +40,31 @@ export default function App(){
 {isShown && (
   <div>
     <div>Groups Details</div>
-    <div>{idNumber}</div>
-    
-      <GroupDetails names="Group 1" pointss="15" tasks={task} tasksScores = {tasksScore} />
+   
+  
+      
+      {(() => {
+        if (idNumber==1) {
+          return (
+            <div><GroupDetails names="Group 1" pointss="15" tasks={task1} tasksScores = {tasksScore1} /></div>
+          )
+        }  if (idNumber==2) {
+          return (
+            <div><GroupDetails names="Group 2" pointss="25" tasks={task2} tasksScores = {tasksScore2} /></div>
+          )
+        }
+        if (idNumber==3) {
+          return (
+            <div><GroupDetails names="Group 3" pointss="5" tasks={task3} tasksScores = {tasksScore3} /></div>
+          )
+        }
+        
+      })()}
   </div>
 )}
 
 {/* 👇️ show component on click */}
-{isShown && <Box />};
+{isShown && <Box />}
   </div>
 }
 
