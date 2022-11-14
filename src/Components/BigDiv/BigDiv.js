8194bd4713970e1d6756918a4eab9fda020b798d
jndgoncalves <joao.n.d.goncalves@gmail.com>
2022-11-14 22:02:46 +0000

    clean App.js

---
+++ src/Components/BigDiv/BigDiv.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import GroupCard from '../Group/GroupCard';
+import Header from '../Header/Header';
+import NewGroup from '../NewGroup/NewGroup.js';
+import AddPoints from '../AddPoints/AddPoints.js';
+import GroupDetails from '../GroupDetails/GroupDetails';
+import '../../App.css';
+
+
+const task1 = ["HTML", "React", "JS", "c++"];
+const tasksScore1 = [11, 22, 33, 41];
+const task2 = ["HTML", "React", "JS", "c++"];
+const tasksScore2 = [44, 14, 54, 56];
+const task3 = ["HTML", "React", "JS", "c++"];
+const tasksScore3 = [77, 33, 76, 99];
+const task4 = ["HTML", "React", "JS", "c++"];
+const tasksScore4 = [33, 66, 32, 21];
+let idNumber
+
+export default function BigDiv() {
+
+  const clickHandler = () => {
+
+  }
+
+  const [isShown, setIsShown] = useState(false);
+  const handleClick = event => {
+    setIsShown(current => !current);
+    idNumber = event.currentTarget.id;
+  };
+
+  const addGroupHandler = (group) => {
+    console.log(group)
+  }
+
+  return (
+    <>
+      <div className="App" id="App">
+        <Header />
+        <div className="BigDiv" id="BigDiv">
+          <button onClick={handleClick} id="1"><GroupCard name="Group 1" points="100" assignment="2"></GroupCard></button>
+          <button onClick={handleClick} id="2"><GroupCard name="Group 2" points="100" assignment="2"></GroupCard></button>
+          <button onClick={handleClick} id="3"><GroupCard name="Group 3" points="100" assignment="2"></GroupCard></button>
+          <button onClick={handleClick} id="4"><GroupCard name="Group 4" points="100" assignment="2"></GroupCard></button>
+        </div>
+        <div className="ResultsDiv" id="Results">
+          <NewGroup onAddGroup={addGroupHandler} />
+          <AddPoints />
+          {isShown && (
+            <div className="ResultsDiv" id="Details">
+              {(() => {
+                { console.log(idNumber) }
+                if (idNumber === "1") {
+                  return (
+                    <div><GroupDetails names="Group 1" pointss="95" tasks={task1} tasksScores={tasksScore1} /></div>
+                  )
+                } if (idNumber === "2") {
+                  return (
+                    <div><GroupDetails names="Group 2" pointss="125" tasks={task2} tasksScores={tasksScore2} /></div>
+                  )
+                }
+                if (idNumber === "3") {
+                  return (
+                    <div><GroupDetails names="Group 3" pointss="59" tasks={task3} tasksScores={tasksScore3} /></div>
+                  )
+                }
+                if (idNumber === "4") {
+                  return (
+                    <div><GroupDetails names="Group 4" pointss="68" tasks={task4} tasksScores={tasksScore4} /></div>
+                  )
+                }
+              })()}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
--- src/Components/GroupDetails/GroupDetails.js
+++ src/Components/GroupDetails/GroupDetails.js
@@ -2,31 +2,31 @@ import "./GroupDetails.css";
 
 const GroupDetails = (props) => {
 
-    const groupName = props.names
-    const points = props.pointss
-    const tasks = props.tasks
-    const tasksScores = props.tasksScores
- 
-    const listItems = tasks.map((task) =>  
+  const groupName = props.names
+  const points = props.pointss
+  const tasks = props.tasks
+  const tasksScores = props.tasksScores
+
+  const listItems = tasks.map((task) =>
     <div><li>{task}</li>
     </div>
   );
-  
+
   const listItems2 = tasksScores.map((score) =>
     <div><li>{score}</li></div>
-  
-);
+
+  );
 
 
-    return <div>
-            <div>
-            <h1>Solera Teams</h1>
-            <span>{groupName} </span>
-            <span>{points} Points</span>
-            </div>
-            <ul>{listItems}</ul>
-            <ul>{listItems2}</ul>
-        </div>
+  return <div>
+    <div>
+      <h1>Solera Teams</h1>
+      <span>{groupName} </span>
+      <span>{points} Points</span>
+    </div>
+    <ul>{listItems}</ul>
+    <ul>{listItems2}</ul>
+  </div>
 }
 
 export default GroupDetails;
