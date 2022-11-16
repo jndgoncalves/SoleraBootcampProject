39bcb6b0744f8660840478383eda5720ed70dd05
ntaquelim <NunoJoao.Sousa@solera.com>
2022-11-16 12:50:21 +0100

    pop up details done

--- src/Components/BigDiv/BigDiv.js
+++ src/Components/BigDiv/BigDiv.js
@@ -4,6 +4,8 @@ import GroupDetails from '../GroupDetails/GroupDetails';
 import Group from '../Group/Group';
 import './BigDiv.css';
 
+import Popup from '../Popup/Popup';
+
 
 
 const task1 = ["HTML", "React", "JS", "c++"];
@@ -14,12 +16,14 @@ const task3 = ["HTML", "React", "JS", "c++"];
 const tasksScore3 = [77, 33, 76, 99];
 const task4 = ["HTML", "React", "JS", "c++"];
 const tasksScore4 = [33, 66, 32, 21];
-let idNumber
+let idNumber;
 let groupList =[
   {id: 1, name: 'Group 1', points: '100', assignment:'5', tasks:task1, score:tasksScore1},
   {id: 2, name: 'Group 2', points: '150', assignment:'8', tasks:task2, score:tasksScore2},
   {id: 3, name: 'Group 3', points: '130', assignment:'6', tasks:task3, score:tasksScore3},
-  {id: 4, name: 'Group 4', points: '95', assignment:'3', tasks:task4, score:tasksScore4}];
+  {id: 4, name: 'Group 4', points: '95', assignment:'3', tasks:task4, score:tasksScore4},
+  {id: 5, name: 'Group 3', points: '130', assignment:'6', tasks:task3, score:tasksScore3},
+  {id: 6, name: 'Group 3', points: '130', assignment:'6', tasks:task3, score:tasksScore3}];
 export default function BigDiv() {
 
   const clickHandler = () => {
@@ -27,10 +31,12 @@ export default function BigDiv() {
   }
 
   const [isShown, setIsShown] = useState(false);
+  const [buttonPopup, setButtonPopup] = useState(false);
 
   const handleClick = event => {
+    setButtonPopup(true);
     setIsShown(current => !current);
-    idNumber=event.currentTarget.id;
+    idNumber=parseInt(event.currentTarget.id);
     const groupDetail = {
       id: event.currentTarget.id,
       name: event.currentTarget.name,
@@ -45,7 +51,7 @@ export default function BigDiv() {
   }
 
   return (
-      <div className="App" id="App">
+      <>
         <div className="BigDiv" id="BigDiv">
 
           {groupList.map((groupList) => 
@@ -57,27 +63,17 @@ export default function BigDiv() {
             assignment={groupList.assignment}
             /></button>
             ))}
-    
-      </div> 
-
-        <div className="ResultsDiv" id="Results">
-          {isShown && (
-            <div className="ResultsDiv" id="Details">
-              {(() => {
-                return (
-                <div className="DetailsDiv">
+        </div> 
+         <Popup trigger = {buttonPopup} setTrigger={setButtonPopup} id={Popup}>
                   <GroupDetails 
-                    //key={groupList[idNumber-1].id}
-                    name={groupList[idNumber-1].name} 
-                    points={groupList[idNumber-1].points} 
-                    task={groupList[idNumber-1].tasks} 
-                    taskScore={groupList[idNumber-1].score}
-                    /></div>
-                )
-              })()}
-            </div>
-          )}
-        </div>
-    </div>
+                    key={groupList[idNumber-1]?.id}
+                    name={groupList[idNumber-1]?.name} 
+                    points={groupList[idNumber-1]?.points} 
+                    task={groupList[idNumber-1]?.tasks} 
+                    taskScore={groupList[idNumber-1]?.score}
+                    />
+         </Popup>         
+        
+    </>
   )
 }
